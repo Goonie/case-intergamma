9e7abc6e15cb26e89ab0d00a3ee01c1343e76288
Paul Martens <paul.martens@divotion.com>
2023-01-18 15:47:54 +0100

    Added Wishlist button to Product component

--- src/ui-components/Product.tsx
+++ src/ui-components/Product.tsx
@@ -1,12 +1,26 @@
 import type { Product as ProductProps } from 'services/products';
+import { useWishlist } from 'services/wishlist';
 
 import './Product.css';
 
 function Product({ id, name, price, photo, description }: ProductProps) {
+  const { addToWishlist } = useWishlist();
+
   return (
     <article className="Product">
       <h1>{name}</h1>
       <img src={'/product-photos/' + photo} />
+
+      <div className="options">
+        <button
+          onClick={() => {
+            addToWishlist({ id, name, price, photo });
+          }}
+        >
+          Add to Wishlist
+        </button>
+      </div>
+
       <p>{description}</p>
     </article>
   );
